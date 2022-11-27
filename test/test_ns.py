ca90d2dbdd87366df3bcf74d95a5dc8b2e62c988
Brenton Cleeland <brenton@brntn.me>
2022-11-27 14:49:49 +1100

    fix: broken test

--- test/test_ns.py
+++ test/test_ns.py
@@ -8,7 +8,12 @@ class NameserversTestCase(TestCase):
         # this should make an additional dns request
         responses = {
             "dns_ns_response": Response(None, None, {"Answer": []}, None, None, None, None),
-            "dns_ns_response_fld": Response(None, None, {"Answer": ["ns1.example.com", "ns2.example.com", "ns3.example.com", "ns3.example.com"]})
+            "dns_ns_response_fld": Response(None, None, {"Answer": [
+                {"data": "ns1.example.com"},
+                {"data": "ns2.example.com"},
+                {"data": "ns3.example.com"},
+                {"data": "ns3.example.com"}
+            ]}, None, None, None, None)
         }
         result = check_at_least_two_nameservers_configured(responses, domain="subdomain.example.com", print_output=False)
         self.assertTrue(result.passed)
